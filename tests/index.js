87bb60bb18d72f6d087ddf1312fae26861e7e9d1
Catskillet <italo.costa@catskillet.com>
2021-08-26 12:10:41 -0300

    fix: add command line

--- tests/index.js
+++ tests/index.js
@@ -2,25 +2,26 @@ require('dotenv').config();
 require('chromedriver');
 const { Builder, By, Key, until } = require('selenium-webdriver');
 
-(async function myFunction() {
-	let driver = await new Builder().forBrowser('chrome').build()
-	try {
-		// Navigate to Url
-		await driver.get(process.env.BROWSER_URL)
+(async () => {
+  const driver = await new Builder().forBrowser('chrome').build();
+  try {
+    // Navigate to Url
+    await driver.get(process.env.BROWSER_URL);
 
-		// Enter text "cheese" and perform keyboard action "Enter"
-		await driver.findElement(By.name('q')).sendKeys('cheese', Key.ENTER)
+    // Enter text "cheese" and perform keyboard action "Enter"
+    await driver.findElement(By.name('q')).sendKeys('cheese', Key.ENTER);
 
-		let firstResult = await driver.wait(
-			until.elementLocated(By.css('h3')),
-			10000
-		)
+    const firstResult = await driver.wait(
+        until.elementLocated(By.css('h3')),
+        10000,
+    );
 
-		console.log(await firstResult.getAttribute('textContent'))
-	} catch (e) {
-		console.error(e)
-	} finally {
-		await driver.quit()
-	}
-})()
-console.log('Test Browser Finished')
+    console.log(await firstResult.getAttribute('textContent'));
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await driver.quit();
+  }
+})();
+
+console.log('Test Browser Finished');
